e43181aae0b1673059aa4761414934763abe8636
sharathMSk <sharatk@microsoft.com>
2017-01-17 12:09:49 +0530

    Asciifying

--- app.py
+++ app.py
@@ -2,38 +2,31 @@
 import web
 import xml.etree.ElementTree as ET
 from pymongo import MongoClient
-
+import json
 uri = "mongodb://gojek-sharath:QbXmwBtPdYeG4bwKMWwElvR3xER7NLQycaD5GXpIoUvoYk2OoXKRK2OjKrycNpMD21h8Vs3HozFYkPs1dc97vg==@gojek-sharath.documents.azure.com:10250/?ssl=true&ssl_cert_reqs=CERT_NONE"
 client = MongoClient(uri)
 db=client.test1_database
 posts = db.posts
 urls = (
-    '/users', 'list_users',
     '/users/(.*)', 'get_user',
     '/add_route', 'add_route'
 )
 
 app = web.application(urls, globals())
 
-class list_users:
-    def GET(self):
-	output = 'users:[';
-	for child in root:
-                print 'child', child.tag, child.attrib
-                output += str(child.attrib) + ','
-	output += ']';
-        return output
-
 class get_user:
     def GET(self, user):
-        ret_data = posts.find({"user_id": user})
-        return ret_data
+        print str(user)
+        ret_data = posts.find_one({"user_id": str(user)})
+        print str(ret_data).encode('ascii','ignore')
+        return str(ret_data).replace('u\'','\"').replace('\'','\"')
 
 class add_route:
     def POST(self):
-        post_data = web.input()
-        print post_data
-        posts.insert_one(post_data).inserted_id
+        post_data = web.data()
+        print str(post_data)
+        obj = json.loads(str(post_data))
+        return posts.insert_one(obj).inserted_id
 
 if __name__ == "__main__":
     app.run()
